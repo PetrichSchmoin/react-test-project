95b545c47c2ff61a944b02a597f920677ce8a0c9
Petrich Schmoin <p.vdm@mailbox.org>
2020-04-10 19:08:28 +0200

    Adds react router

--- apps/my-app/src/ui/App.tsx
+++ apps/my-app/src/ui/App.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Logo } from './components/Logo';
 import { globals } from './globals/globals';
+import { Route, Switch } from 'react-router';
 
 const App: React.FC = () => (
   <div className={'App'}>
     <Navbar bg={globals.colorTheme} variant={globals.colorTheme}>
-      <Navbar.Brand href={'#home'}>
+      <Navbar.Brand href={'home'}>
         <Logo />
         React Test App
       </Navbar.Brand>
     </Navbar>
+    <Switch>
+      <Route path={'/home'}>
+        <div>Hello Home!</div>{' '}
+      </Route>
+    </Switch>
   </div>
 );
 
